f27d75f86500a2ecc9359075de77cf3995c413e8
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2021-05-28 14:25:58 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+    
+]
+DEFAULT_FROM_EMAIL = 'cassandratalbot@yahoo.co.uk'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
