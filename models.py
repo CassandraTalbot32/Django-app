69d142819f724b765e817b2b347484fa8374b774
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2021-05-28 14:12:47 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.urls import reverse
 
 # Create your models here.
-class Product(models.Model):
-	title		= models.CharField(max_length=120)
-	description = models.TextField(blank=True, null=True)
-	price		= models.DecimalField(decimal_places=2, max_digits=10)
-	summary		= models.TextField()
-	date		= models.DateField(null=True)
-	contact		= models.EmailField(max_length=254)
-	featured	= models.NullBooleanField()
+class LSH(models.Model):
+	Name		= models.CharField(label=max_length=120)
+	Email 		= models.EmailField(blank=True, null=True)
+	Message		= models.CharField(decimal_places=2, max_digits=10)
+	Date		= models.DateField(null=True)
+
+
+
 	
-	def get_absolute_url(self):
-		return reverse("products:product-detail", kwargs={"id": self.id})
+
+
